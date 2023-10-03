d7f7afb8587a6453a8a835e0b5f16e2e61398389
SenoritaMari <mari.samoylenko.05@mail.ru>
2023-10-03 16:07:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/MashaMetod.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MashaMetod
+{
+    public class Avto
+    {
+        private string nom;//номер
+        private float bak;//колличество бензина в баке
+        private float ras;// расход на 100
+
+        public void info(string nom, float bak, float ras)
+        {
+            this.nom = nom;
+            this.bak = bak;
+            this.ras = ras;
+        }
+        public void Outinfo()
+        {
+            Console.WriteLine($"Номер: {this.nom}\nБак: {this.bak} \nРасход: {this.ras}");
+        }
+        public void Zapravka(float top)
+        {
+            this.bak += top;
+        }
+        public void Move(float km)
+        {
+            float ostatok = this.bak - (km * (this.ras / 100));
+            if (ostatok > 0)
+            {
+                Console.WriteLine($"Мы доехали до места назначения и в баке осталось: {ostatok:F2}");
+                Console.WriteLine($"Пункт назначения: {km} км");
+            }
+            if (ostatok <= 0)
+            {
+                Console.WriteLine($"Пункт назначения: {km} км");
+                Console.WriteLine("Сколько заправим топлива: ");
+                Zapravka(Convert.ToSingle(Console.ReadLine()));
+                ostatok = this.bak - (km * (this.ras / 100));
+                if (ostatok > 0)
+                {
+                    Console.WriteLine($"Мы доехали до места назначения и в баке осталось: {ostatok:F2}");
+                    Console.WriteLine($"Пункт назначения: {km} км");
+                }
+                if (ostatok < 0)
+                {
+                    Console.WriteLine($"Пункт назначения: {km} км");
+                    Console.WriteLine($"Не хватило до пункта назначения {ostatok * -1}");
+
+                }
+
+                
+            }
+        }
+        public void Octatok(float Octatok)
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using MashaMetod;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Avto[] car = new Avto[6];
+        Random random = new Random();
+
+        for (int i = 0; i < car.Length; i++)
+        {
+        Console.WriteLine("Введи количество топлива в баке: ");
+            car[i] = new Avto();
+            car[i].info("222", float.Parse(Console.ReadLine()), 10);
+            car[i].Outinfo();
+            float dal = random.Next(1, 300);
+            car[i].Move(dal);
+        }
+        /*car.info("222", float.Parse(Console.ReadLine()), 10);
+        car.Outinfo();
+        float dal = random.Next(1, 300);
+        car.Move(dal);*/
+        
+
+        
+    }
+}
